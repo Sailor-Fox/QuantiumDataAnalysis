cf9429341e224e322874ec9db62eb5eabb9e63e6
Sailor-Fox <sailorfox2728@outlook.com>
2023-09-13 14:11:01 +1000

    working on data cleaning
    slash data verification

--- quantium.R
+++ quantium.R
@@ -1,23 +1,34 @@
 library(tidyverse)
-purchase <- read_csv("QVI_purchase_behaviour.csv")
-transac <- read_csv("QVI_transaction_data.csv")
-view(purchase)
-view(transac)
+customerData <- read_csv("QVI_purchase_behaviour.csv")
+transactionData <- read_csv("QVI_transaction_data.csv")
+view(customerData)
+view(transactionData)
+str(customerData)
+str(transactionData)
+
+# convert date to DATE data type
+transactionData$DATE <- as.Date(transactionData$DATE, origin = "1899-12-30")
+
+# summary of PROD_NAME column
+view(transactionData$PROD_NAME)
+
+productWords <- tibble(unlist(strsplit(unique(transactionData[, PROD_NAME]), "")))
+
 
 # some initial visualisation of the purchasers
-purchase %>%
+customerData %>%
   ggplot(aes(x = LIFESTAGE, fill = PREMIUM_CUSTOMER)) +
   geom_bar() +
   theme_bw() +
   labs(title = "Number of customers in each lifestage")
 
-purchase %>% 
+customerData %>% 
   ggplot(aes(x = PREMIUM_CUSTOMER, fill = LIFESTAGE)) +
   geom_bar() +
   theme_bw() +
   labs(title = "Number of type of customer")
 
-purchase %>% 
+customerData %>% 
   ggplot(aes(x=PREMIUM_CUSTOMER)) +
   geom_bar() +
   facet_wrap(~LIFESTAGE) +
