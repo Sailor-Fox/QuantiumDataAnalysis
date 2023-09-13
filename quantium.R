4251d21d8a61f3f751d8ab7f9dbc7cb349ec88db
Sailor-Fox <sailorfox2728@outlook.com>
2023-09-13 13:46:38 +1000

    intial visaulisations

--- quantium.R
+++ quantium.R
@@ -1,3 +1,25 @@
 library(tidyverse)
-QVI_purchase_behaviour <- read_csv("QVI_purchase_behaviour.csv")
-QVI_transanction_data <- read_csv("QVI_purchase_behaviour.csv")
+purchase <- read_csv("QVI_purchase_behaviour.csv")
+transac <- read_csv("QVI_transaction_data.csv")
+view(purchase)
+view(transac)
+
+# some initial visualisation of the purchasers
+purchase %>%
+  ggplot(aes(x = LIFESTAGE, fill = PREMIUM_CUSTOMER)) +
+  geom_bar() +
+  theme_bw() +
+  labs(title = "Number of customers in each lifestage")
+
+purchase %>% 
+  ggplot(aes(x = PREMIUM_CUSTOMER, fill = LIFESTAGE)) +
+  geom_bar() +
+  theme_bw() +
+  labs(title = "Number of type of customer")
+
+purchase %>% 
+  ggplot(aes(x=PREMIUM_CUSTOMER)) +
+  geom_bar() +
+  facet_wrap(~LIFESTAGE) +
+  theme_bw() +
+  labs(title = "Type of customer by lifestage")
